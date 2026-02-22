eb9931de5b5d23ca5803ba97f41205ccb707fe8c
maharanianindya <anindyabudiarti@gmail.com>
2026-02-22 15:44:57 +0700

    fix PMD error in eshopapplication

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SuppressWarnings("PMD.ClassWithOnlyPrivateConstructorsShouldBeFinal")
 public class EshopApplication {
 
     private EshopApplication() {
         // Prevent instantiation
     }
+
     public static void main(String[] args) {
         SpringApplication.run(EshopApplication.class, args);
     }
